ff10161fcb00ced0a5eb415023274263d71babc6
Aleksandr Kazachkov <akazachk@users.noreply.github.com>
2021-04-06 21:58:47 -0400

    Added some comments in run_test

--- test/run_test.sh
+++ test/run_test.sh
@@ -16,4 +16,7 @@ then
   fi
 fi
 
+# Cbc: --bb_runs=1 --bb_mode=11 --bb_strategy=528
+# CPLEX: --bb_runs=1 --bb_mode=11 --bb_strategy=532
+# Gurobi: --bb_runs=1 --bb_mode=11 --bb_strategy=536
 ${VPC_DIR}/Debug/vpc -f ${VPC_DIR}/test/bm23.mps -d 2 --optfile=${VPC_DIR}/data/ip_obj.csv $1 $2 $3 $4 $5 $6 $7 $8
