10e5990020d0aced16904985e57afdc23c29914a
Aleksandr Kazachkov <>
2021-05-14 01:07:21 -0400

    Added prepare_batch script to be called with GNU parallel (nohup parallel -j 5 < job_list.txt)

---
+++ scripts/prepare_batch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -z "$PROJ_DIR" ]
+then
+  if [ ! -z "${REPOS_DIR}" ]
+  then
+    echo "Please define PROJ_DIR (the root project dir, possibly ${REPOS_DIR}/vpc):"
+  else
+    echo "Please define PROJ_DIR (the root project dir):"
+  fi
+  read PROJ_DIR
+  if [ -z "$PROJ_DIR" ]
+    then echo "Need to define PROJ_DIR. Exiting."
+    exit 1
+  fi
+fi
+
+export SCRIPT_DIR=${PROJ_DIR}/scripts
+export INSTANCE_DIR=${PROJ_DIR}/data/instances
+export INSTANCE_LIST=${SCRIPT_DIR}/slurm/small_original.instances
+export RESULTS_DIR=${PROJ_DIR}/results
+
+TASK_ID=0
+while read line; do
+  TASK_ID=$((TASK_ID+1))
+
+  # Skip empty lines
+  if [ -z "$line" ]; then
+    continue
+  fi
+
+  #nohup python3 ${SCRIPT_DIR}/scip_test.py ${INSTANCE_DIR}/$line $PROJ_DIR/results &
+  echo "Preparing command to run instance $line (task $TASK_ID) at `date`"
+  echo "nohup /usr/bin/time -v ${SCRIPT_DIR}/run_experiments.sh ${INSTANCE_DIR}/$line.mps $RESULTS_DIR/bb/${TASK_ID} bb 2>&1" >> job_list_bb.txt
+  echo "nohup /usr/bin/time -v ${SCRIPT_DIR}/run_experiments.sh ${INSTANCE_DIR}/$line.mps $RESULTS_DIR/bb0/${TASK_ID} bb0 2>&1" >> job_list_bb0.txt
+done < ${INSTANCE_LIST}
+
