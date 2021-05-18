0f70354f2a7062119bda497e84f38a531b1a2d36
Aleksandr Kazachkov <>
2021-05-18 14:44:07 -0400

    Added realpath and date to prepare batch.

--- scripts/prepare_batch.sh
+++ scripts/prepare_batch.sh
@@ -15,9 +15,10 @@ then
   fi
 fi
 
+export PROJ_DIR=`realpath -s ${PROJ_DIR}`
 export SCRIPT_DIR=${PROJ_DIR}/scripts
 export INSTANCE_DIR=${PROJ_DIR}/data/instances
-export INSTANCE_LIST=${SCRIPT_DIR}/slurm/small_presolved.instances
+export INSTANCE_LIST=${SCRIPT_DIR}/small_presolved.instances
 export RESULTS_DIR=${PROJ_DIR}/results
 
 TASK_ID=0
@@ -32,9 +33,10 @@ while read line; do
   fi
 
   CASE_NUM=`printf %03d $TASK_ID`
+  STUB=`date +%F`
   echo "Preparing command to run instance $line (task $TASK_ID) at `date`"
-  echo "nohup /usr/bin/time -v ${SCRIPT_DIR}/run_experiments.sh ${INSTANCE_DIR}/$line.mps $RESULTS_DIR/bb/${CASE_NUM} bb 2>&1" >> job_list_bb.txt
-  echo "nohup /usr/bin/time -v ${SCRIPT_DIR}/run_experiments.sh ${INSTANCE_DIR}/$line.mps $RESULTS_DIR/bb0/${CASE_NUM} bb0 2>&1" >> job_list_bb0.txt
-  #echo "nohup /usr/bin/time -v ${SCRIPT_DIR}/run_experiments.sh ${INSTANCE_DIR}/$line.mps $RESULTS_DIR/preprocess/${CASE_NUM} preprocess 2>&1" >> job_list_preprocess.txt
+  echo "nohup /usr/bin/time -v ${SCRIPT_DIR}/run_experiments.sh ${INSTANCE_DIR}/$line.mps $RESULTS_DIR/$STUB/bb/${CASE_NUM} bb 2>&1" >> job_list_bb.txt
+  echo "nohup /usr/bin/time -v ${SCRIPT_DIR}/run_experiments.sh ${INSTANCE_DIR}/$line.mps $RESULTS_DIR/$STUB/bb0/${CASE_NUM} bb0 2>&1" >> job_list_bb0.txt
+  #echo "nohup /usr/bin/time -v ${SCRIPT_DIR}/run_experiments.sh ${INSTANCE_DIR}/$line.mps $RESULTS_DIR/$STUB/preprocess/${CASE_NUM} preprocess 2>&1" >> job_list_preprocess.txt
 done < ${INSTANCE_LIST}
 
