221fbec5680f759bd90374834b56f697be7112ee
Aleksandr Kazachkov <>
2021-05-22 18:50:40 -0400

    Add moving of CPLEX-presolved-solutions

--- scripts/move_preprocessed.sh
+++ scripts/move_preprocessed.sh
@@ -15,7 +15,7 @@ then
   fi
 fi
 
-SOLVER_TYPE="gurobi"
+SOLVER_TYPE="cplex"
 SRC_DIR="${VPC_DIR}/data/instances/original"
 DEST_DIR="${VPC_DIR}/data/instances/presolved_${SOLVER_TYPE}"
 
@@ -23,33 +23,54 @@ STUB="miplib2"
 echo "Copying preprocessed instances from ${SRC_DIR}/${STUB} to ${DEST_DIR}/${STUB}"
 mkdir -p ${DEST_DIR}/${STUB}
 mv ${SRC_DIR}/${STUB}/*_presolved.mps* ${DEST_DIR}/${STUB}
+if [ $SOLVER_TYPE = "cplex" ]; then
+  mv ${SRC_DIR}/${STUB}/*_presolved.pre ${DEST_DIR}/${STUB}
+fi
 
 STUB="miplib3"
 echo "Copying preprocessed instances from ${SRC_DIR}/${STUB} to ${DEST_DIR}/${STUB}"
 mkdir -p ${DEST_DIR}/${STUB}
 mv ${SRC_DIR}/${STUB}/*_presolved.mps* ${DEST_DIR}/${STUB}
+if [ $SOLVER_TYPE = "cplex" ]; then
+  mv ${SRC_DIR}/${STUB}/*_presolved.pre ${DEST_DIR}/${STUB}
+fi
 
 STUB="miplib2003"
 echo "Copying preprocessed instances from ${SRC_DIR}/${STUB} to ${DEST_DIR}/${STUB}"
 mkdir -p ${DEST_DIR}/${STUB}
 mv ${SRC_DIR}/${STUB}/*_presolved.mps* ${DEST_DIR}/${STUB}
+if [ $SOLVER_TYPE = "cplex" ]; then
+  mv ${SRC_DIR}/${STUB}/*_presolved.pre ${DEST_DIR}/${STUB}
+fi
 
 STUB="miplib2010"
 echo "Copying preprocessed instances from ${SRC_DIR}/${STUB} to ${DEST_DIR}/${STUB}"
 mkdir -p ${DEST_DIR}/${STUB}
 mv ${SRC_DIR}/${STUB}/*_presolved.mps* ${DEST_DIR}/${STUB}
+if [ $SOLVER_TYPE = "cplex" ]; then
+  mv ${SRC_DIR}/${STUB}/*_presolved.pre ${DEST_DIR}/${STUB}
+fi
 
 STUB="miplib2017"
 echo "Copying preprocessed instances from ${SRC_DIR}/${STUB} to ${DEST_DIR}/${STUB}"
 mkdir -p ${DEST_DIR}/${STUB}
 mv ${SRC_DIR}/${STUB}/*_presolved.mps* ${DEST_DIR}/${STUB}
+if [ $SOLVER_TYPE = "cplex" ]; then
+  mv ${SRC_DIR}/${STUB}/*_presolved.pre ${DEST_DIR}/${STUB}
+fi
 
 STUB="coral"
 echo "Copying preprocessed instances from ${SRC_DIR}/${STUB} to ${DEST_DIR}/${STUB}"
 mkdir -p ${DEST_DIR}/${STUB}
 mv ${SRC_DIR}/${STUB}/*_presolved.mps* ${DEST_DIR}/${STUB}
+if [ $SOLVER_TYPE = "cplex" ]; then
+  mv ${SRC_DIR}/${STUB}/*_presolved.pre ${DEST_DIR}/${STUB}
+fi
 
 STUB="neos"
 echo "Copying preprocessed instances from ${SRC_DIR}/${STUB} to ${DEST_DIR}/${STUB}"
 mkdir -p ${DEST_DIR}/${STUB}
 mv ${SRC_DIR}/${STUB}/*_presolved.mps* ${DEST_DIR}/${STUB}
+if [ $SOLVER_TYPE = "cplex" ]; then
+  mv ${SRC_DIR}/${STUB}/*_presolved.pre ${DEST_DIR}/${STUB}
+fi
