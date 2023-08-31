9d162e8091ec1fcc4b02d5f53fd1ca0dd797a56d
Aleksandr Kazachkov <akazachk@users.noreply.github.com>
2023-08-31 16:24:11 -0400

    Updated get_instances with new links and paths

--- setup/get_instances.sh
+++ setup/get_instances.sh
@@ -27,56 +27,65 @@ echo "Instances will be downloaded to $DATA_DIR"
 START_DIR=$PWD
 cd $DATA_DIR
 
-echo "Downloading MIPLIB2017"
-wget http://miplib2017.zib.de/downloads/collection.zip
-mkdir -p ${DATA_DIR}/miplib2017
-unzip collection.zip -d ${DATA_DIR}/miplib2017
-mv ${DATA_DIR}/miplib2017/mas74.mps.gz ${DATA_DIR}/miplib2017/mas074.mps.gz
-mv ${DATA_DIR}/miplib2017/mas76.mps.gz ${DATA_DIR}/miplib2017/mas076.mps.gz
+INSTANCES=miplib2017
+echo "Downloading ${INSTANCES}"
+wget -O ${INSTANCES}.zip 'https://miplib.zib.de/downloads/collection.zip'
+mkdir -p ${DATA_DIR}/${INSTANCES}
+unzip -q ${INSTANCES}.zip -d ${DATA_DIR}/${INSTANCES}
+mv ${DATA_DIR}/${INSTANCES}/revised-submissions/*/instances/*.mps.gz ${DATA_DIR}/${INSTANCES}
+#mv ${DATA_DIR}/${INSTANCES}/mas74.mps.gz ${DATA_DIR}/miplib2017/mas074.mps.gz
+#mv ${DATA_DIR}/${INSTANCES}/mas76.mps.gz ${DATA_DIR}/miplib2017/mas076.mps.gz
 
-echo "Downloading MIPLIB2010"
-wget http://miplib2010.zib.de/download/miplib2010-complete.zip
-unzip miplib2010-complete.zip -d ${DATA_DIR}
+# In MIPLIB 2010, unzipping will create desired directory
+INSTANCES=miplib2010
+echo "Downloading ${INSTANCES}"
+wget -o ${INSTANCES}.zip 'https://miplib2010.zib.de/download/miplib2010-1.1.3-benchmark.zip'
+unzip -q ${INSTANCES}.zip -d ${DATA_DIR}
 
-echo "Downloading MIPLIB2003"
-wget http://miplib2010.zib.de/miplib2003/download/miplib2003.tar
-mkdir -p ${DATA_DIR}/miplib2003
-tar -xvf miplib2003.tar --directory ${DATA_DIR}/miplib2003
+INSTANCES=miplib2003
+echo "Downloading ${INSTANCES}"
+wget https://miplib2010.zib.de/miplib2003/download/miplib2003.tar
+mkdir -p ${DATA_DIR}/${INSTANCES}
+tar -xf ${INSTANCES}.tar --directory ${DATA_DIR}/${INSTANCES}
 
-echo "Downloading MIPLIB3"
-wget http://miplib2010.zib.de/miplib3/miplib3.tar.gz
-tar -xvf miplib3.tar.gz --directory ${DATA_DIR}
-rm ${DATA_DIR}/miplib3/miplib.cat
-rm ${DATA_DIR}/miplib3/mps_format
-rm ${DATA_DIR}/miplib3/references
-cd ${DATA_DIR}/miplib3
+INSTANCES=miplib3
+echo "Downloading ${INSTANCES}"
+wget https://miplib2010.zib.de/miplib3/miplib3.tar.gz
+tar -xf ${INSTANCES}.tar.gz --directory ${DATA_DIR}
+rm ${DATA_DIR}/${INSTANCES}/miplib.cat
+rm ${DATA_DIR}/${INSTANCES}/mps_format
+rm ${DATA_DIR}/${INSTANCES}/references
+cd ${DATA_DIR}/${INSTANCES}
 for f in *; do mv "$f" "$f.mps"; done
 for f in *; do gzip "$f"; done
 cd ${DATA_DIR}
-cp ${DATA_DIR}/miplib2/stein27_nocard.mps.gz ${DATA_DIR}/miplib3/
-cp ${DATA_DIR}/miplib2/stein45_nocard.mps.gz ${DATA_DIR}/miplib3/
 
-#echo "Downloading MIPLIB2"
-#wget http://miplib2010.zib.de/miplib2/miplib.tar.gz
-#mkdir ${DATA_DIR}/miplib2
-#tar -xvf miplib.tar.gz --directory ${DATA_DIR}/miplib2
-#rm ${DATA_DIR}/miplib2/miplib.cat
-#rm ${DATA_DIR}/miplib2/miplib/mps_format
-#rm ${DATA_DIR}/miplib2/miplib/references
-#cd ${DATA_DIR}/miplib2/miplib
-#for f in *; do mv "$f" "$f.mps"; done
-#for f in *; do gzip "$f"; done
+INSTANCES=miplib2
+echo "Downloading ${INSTANCES}"
+wget -O ${INSTANCES}.tar.gz 'https://miplib2010.zib.de/miplib2/miplib.tar.gz'
+mkdir ${DATA_DIR}/${INSTANCES}
+tar -xf ${INSTANCES}.tar.gz --directory ${DATA_DIR}/${INSTANCES}
+mv ${DATA_DIR}/${INSTANCES}/miplib/* ${DATA_DIR}/${INSTANCES}
+rm ${DATA_DIR}/${INSTANCES}/miplib.cat
+rm ${DATA_DIR}/${INSTANCES}/mps_format
+rm ${DATA_DIR}/${INSTANCES}/references
+cd ${DATA_DIR}/${INSTANCES}
+rm -r miplib
+for f in *; do mv "$f" "$f.mps"; done
+for f in *; do gzip "$f"; done
 #mv *.mps.gz ..
 #cd ..
-#rm -r miplib
-#cd ${DATA_DIR}
+cd ${DATA_DIR}
 
-echo "Downloading COR@L instances"
-wget https://coral.ise.lehigh.edu/wp-content/uploads/mip-instances/instances/ALL_INSTANCE.tar
-mv ALL_INSTANCE.tar coral.tar
-mkdir -p ${DATA_DIR}/coral
-tar -xvf coral.tar --directory ${DATA_DIR}/coral
-mv coral/mcf2.mps.bz2 coral/danoint.mps.bz2
+#cp ${DATA_DIR}/miplib2/stein27_nocard.mps.gz ${DATA_DIR}/miplib3/
+#cp ${DATA_DIR}/miplib2/stein45_nocard.mps.gz ${DATA_DIR}/miplib3/
+
+INSTANCES=coral
+echo "Downloading ${INSTANCES}"
+wget -O ${INSTANCES}.tar https://coral.ise.lehigh.edu/wp-content/uploads/mip-instances/instances/ALL_INSTANCE.tar
+mkdir -p ${DATA_DIR}/${INSTANCES}
+tar -xf ${INSTANCES}.tar --directory ${DATA_DIR}/${INSTANCES}
+mv ${INSTANCES}/mcf2.mps.bz2 ${INSTANCES}/danoint.mps.bz2
 
 echo "Finished downloading instances to $DATA_DIR"
 cd ${START_DIR}
